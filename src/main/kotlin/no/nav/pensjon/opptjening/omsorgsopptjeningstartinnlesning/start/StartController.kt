9410abfde6f121661dbafd7e9c5d36c584017587
eivind <eivind.askestad@nav.no>
2023-01-05 10:12:12 +0100

    Added ar parmeter to endpoint
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjeningstartinnlesning/start/StartController.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjeningstartinnlesning/start/StartController.kt
@@ -1,15 +1,17 @@
 package no.nav.pensjon.opptjening.omsorgsopptjeningstartinnlesning.start
 
 import no.nav.security.token.support.core.api.Protected
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Protected
 class StartController {
 
-    @GetMapping("/start")
-    fun start() {
+    @GetMapping("/start/innlesning/{ar}")
+    fun startInnlesning(@PathVariable ar: String): ResponseEntity<String> =
+        ResponseEntity.ok("""Startet innlesning for $ar""")
 
-    }
 }
