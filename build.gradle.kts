26280d461cb13f7b617a95f10fedfd1cd2cc48eb
Jacob Meidell <jacob.meidell@nav.no>
2023-07-15 12:33:36 +0200

    add actuator dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("no.nav.security:token-validation-spring:$navTokenSupportVersion")
 
     implementation("org.hibernate:hibernate-validator:$hibernateValidatorVersion")
