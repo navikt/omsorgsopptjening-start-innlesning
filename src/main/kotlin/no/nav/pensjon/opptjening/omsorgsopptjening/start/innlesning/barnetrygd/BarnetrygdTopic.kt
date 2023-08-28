141503cfab28622b479a9a3be1b3ceab4694902e
Jacob Meidell <jacob.meidell@nav.no>
2023-08-28 11:45:30 +0200

    Rename db after nuke, set ba urls

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/barnetrygd/BarnetrygdTopic.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/barnetrygd/BarnetrygdTopic.kt
@@ -1,6 +1,6 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.start.innlesning.barnetrygd
 
 object BarnetrygdTopic {
-    const val NAME = "pensjonopptjening.omsorgsarbeid-mock-topic"
-//    const val NAME = "teamfamilie.aapen-familie-ba-sak-identer-med-barnetrygd"
+//    const val NAME = "pensjonopptjening.omsorgsarbeid-mock-topic"
+    const val NAME = "teamfamilie.aapen-familie-ba-sak-identer-med-barnetrygd"
 }
