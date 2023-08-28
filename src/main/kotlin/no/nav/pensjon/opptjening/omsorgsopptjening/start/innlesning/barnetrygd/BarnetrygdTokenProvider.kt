e7471c95446c6e65c0fd238b5f9c3957ce4e8421
Jacob Meidell <jacob.meidell@nav.no>
2023-08-28 13:16:55 +0200

    Request token for appropriate app-id

---
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/barnetrygd/BarnetrygdTokenProvider.kt
@@ -0,0 +1,24 @@
+package no.nav.pensjon.opptjening.omsorgsopptjening.start.innlesning.barnetrygd
+
+import no.nav.pensjon.opptjening.omsorgsopptjening.start.innlesning.config.AzureAdTokenClientConfig
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+import pensjon.opptjening.azure.ad.client.AzureAdConfig
+import pensjon.opptjening.azure.ad.client.AzureAdTokenProvider
+import pensjon.opptjening.azure.ad.client.AzureAdVariableConfig
+import pensjon.opptjening.azure.ad.client.TokenProvider
+
+@Component
+@Profile("dev-gcp", "prod-gcp")
+class BarnetrygdTokenProvider(
+    @Value("\${BARNETRYGD_API_ID}") val appId: String,
+    private val azureAdConfig: AzureAdTokenClientConfig,
+    private val config: AzureAdConfig = AzureAdVariableConfig(
+        azureAppClientId = azureAdConfig.azureAppClientId,
+        azureAppClientSecret = azureAdConfig.azureAppClientSecret,
+        targetApiId = appId,
+        wellKnownUrl = azureAdConfig.wellKnownUrl,
+    ),
+    private val tokenProvider: TokenProvider = AzureAdTokenProvider(config)
+) : TokenProvider by tokenProvider
