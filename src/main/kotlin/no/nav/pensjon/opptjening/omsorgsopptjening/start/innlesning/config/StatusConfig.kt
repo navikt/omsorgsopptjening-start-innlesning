0aeb6d0f394572843b5b07de4b59f405fb4fa3ba
Jan Kroken <jan.kroken@nav.com>
2023-09-29 12:47:11 +0200

    La til profiler på StatusCheckConfig

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/config/StatusConfig.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/config/StatusConfig.kt
@@ -3,10 +3,12 @@ package no.nav.pensjon.opptjening.omsorgsopptjening.start.innlesning.config
 import no.nav.pensjon.opptjening.omsorgsopptjening.start.innlesning.barnetrygd.tasks.StatusCheckTask
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @Configuration
 @EnableScheduling
+@Profile("dev-gcp", "prod-gcp")
 class StatusConfig {
 
     @Bean
