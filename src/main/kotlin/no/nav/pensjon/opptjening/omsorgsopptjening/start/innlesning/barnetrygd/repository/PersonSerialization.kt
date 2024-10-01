a699c130aa607e7871d51df5fd2e9ae8a0c0fde1
Jan Kroken <jan.kroken@nav.no>
2024-10-01 13:23:16 +0200

    fikset serialisering av Person

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/barnetrygd/repository/PersonSerialization.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/barnetrygd/repository/PersonSerialization.kt
@@ -1,15 +1,18 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.start.innlesning.barnetrygd.repository
 
-import com.fasterxml.jackson.module.kotlin.jsonMapper
-import no.nav.pensjon.opptjening.omsorgsopptjening.felles.mapToJson
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.pensjon.opptjening.omsorgsopptjening.start.innlesning.barnetrygd.domain.Person
 
 object PersonSerialization {
+    val objectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
+
     fun Person.toJson(): String {
-        return this.mapToJson()
+        return objectMapper.writeValueAsString(this)
     }
 
     fun String.toPerson(): Person {
-        return jsonMapper().readValue(this, Person::class.java)
+        return objectMapper.readValue<Person>(this)
     }
 }
