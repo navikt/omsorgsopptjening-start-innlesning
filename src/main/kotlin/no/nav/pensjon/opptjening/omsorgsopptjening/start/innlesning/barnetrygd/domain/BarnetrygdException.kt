b15176f4d022d5d1b48bb044920c1c63cd9baf2c
Jan Kroken <jan.kroken@nav.no>
2024-11-15 10:52:43 +0100

    la til identrolle i PersonOppslagException

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/barnetrygd/domain/BarnetrygdException.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/barnetrygd/domain/BarnetrygdException.kt
@@ -1,6 +1,17 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.start.innlesning.barnetrygd.domain
 
+import no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.messages.domene.IdentRolle
+
 sealed class BarnetrygdException(msg: String, cause: Throwable) : RuntimeException(msg, cause) {
-    class FeilVedHentingAvPersonId(val fnr: Ident, msg: String, cause: Throwable) : BarnetrygdException(msg, cause)
-    class OverlappendePerioder(msg: String, cause: Throwable) : BarnetrygdException(msg, cause)
+    class FeilVedHentingAvPersonId(
+        val fnr: Ident,
+        val rolle: IdentRolle,
+        msg: String,
+        cause: Throwable
+    ) : BarnetrygdException(msg, cause)
+
+    class OverlappendePerioder(
+        msg: String,
+        cause: Throwable
+    ) : BarnetrygdException(msg, cause)
 }
