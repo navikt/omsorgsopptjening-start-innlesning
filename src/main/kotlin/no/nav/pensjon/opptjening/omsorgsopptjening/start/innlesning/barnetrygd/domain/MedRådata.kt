fff14c77716cba705cc0053a6d8c56a3f122fa2f
Jan Kroken <jan.kroken@nav.no>
2024-11-26 00:03:37 +0100

    rådata fra pdl-kall (pågående)

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/barnetrygd/domain/MedRådata.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/start/innlesning/barnetrygd/domain/MedRådata.kt
@@ -1,8 +1,8 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.start.innlesning.barnetrygd.domain
 
-import no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.Rådata
+import no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.RådataFraKilde
 
 data class MedRådata<T>(
     val value: T,
-    val rådata: Rådata,
+    val rådata: List<RådataFraKilde>,
 )
